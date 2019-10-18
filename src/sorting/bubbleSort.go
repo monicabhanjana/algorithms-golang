0326c16d58b5d916fca0f5b523ead143167ebe10
Akshansh Bhanjana <akshansh2000@gmail.com>
2019-10-18 09:46:30 +0530

    removed redundant statements

--- src/sorting/bubbleSort.go
+++ src/sorting/bubbleSort.go
@@ -2,15 +2,13 @@ package sorting
 
 import (
 	"custom"
-	"fmt"
 	"time"
 )
 
 func BubbleSort() {
 	var arr = custom.GenerateRandomArray()
 
-	custom.CustomPrint(time.Second, "Randomly generated array:", arr, "\n")
-	fmt.Println()
+	custom.CustomPrint(time.Second, "Randomly generated array:", arr, "\n\n")
 
 	i := 0
 	for i < len(arr) {
--- src/sorting/selectionSort.go
+++ src/sorting/selectionSort.go
@@ -2,15 +2,13 @@ package sorting
 
 import (
 	"custom"
-	"fmt"
 	"time"
 )
 
 func SelectionSort() {
 	var arr = custom.GenerateRandomArray()
 
-	custom.CustomPrint(time.Second, "Randomly generated array:", arr, "\n")
-	fmt.Println()
+	custom.CustomPrint(time.Second, "Randomly generated array:", arr, "\n\n")
 
 	custom.CustomPrint(time.Second, "\nSorted array:", arr, "\n")
 }
