d9795d95d67804cb6637bdef5434785e50d6ca7f
Akshansh Bhanjana <akshansh2000@gmail.com>
2019-10-22 03:53:47 +0530

    added linear search

--- src/sorting/selectionSort.go
+++ src/sorting/selectionSort.go
@@ -8,7 +8,7 @@ import (
 func SelectionSort() {
 	var arr = custom.GenerateRandomArray()
 
-	custom.CustomPrint(time.Second, "Randomly generated array:", arr, "\n\n")
+	custom.CustomPrint(time.Second, "Randomly generated array: ", arr, "\n\n")
 
 	i := 0
 	for i < len(arr) {
@@ -29,5 +29,5 @@ func SelectionSort() {
 		i++
 	}
 
-	custom.CustomPrint(time.Second, "\nSorted array:", arr, "\n")
+	custom.CustomPrint(time.Second, "\nSorted array: ", arr, "\n")
 }
