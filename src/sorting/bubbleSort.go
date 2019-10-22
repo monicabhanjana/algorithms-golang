798cc3080bacce106f70d0abb90b6fe9638e0284
Akshansh Bhanjana <akshansh2000@gmail.com>
2019-10-22 17:02:30 +0530

    made bubble sort more readable

--- src/sorting/bubbleSort.go
+++ src/sorting/bubbleSort.go
@@ -2,11 +2,13 @@ package sorting
 
 import (
 	"custom"
+	"fmt"
 	"time"
 )
 
 func BubbleSort() {
 	var arr = custom.GenerateRandomArray()
+	var previous = false
 
 	custom.CustomPrint(time.Second, "Randomly generated array: ", arr, "\n\n")
 
@@ -15,8 +17,18 @@ func BubbleSort() {
 		j := 0
 		for j < len(arr)-i-1 {
 			if arr[j] > arr[j+1] {
+				if previous {
+					fmt.Println()
+				}
+
+				custom.CustomPrint(150*time.Millisecond, "\nPointer at index ", j, ", found ", arr[j], " > arr[", j, " + 1] = ", arr[j+1], "\n")
+				previous = false
+
 				custom.CustomPrint(150*time.Millisecond, "Swapping arr[", j, "] = ", arr[j], " and arr[", j+1, "] = ", arr[j+1], "\n")
 				arr[j], arr[j+1] = arr[j+1], arr[j]
+			} else {
+				custom.CustomPrint(150*time.Millisecond, "\nPointer at index ", j, ", found ", arr[j], " ≤ arr[", j, " + 1] = ", arr[j+1])
+				previous = true
 			}
 			j++
 		}
